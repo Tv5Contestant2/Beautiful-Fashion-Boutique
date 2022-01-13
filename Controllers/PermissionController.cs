8961e89908bb6a28b826b949c014b02141933254
Michael Pama <michael071788@gmail.com>
2022-01-13 16:41:28 +0400

    roles, permission and claims updated functionality

--- Controllers/PermissionController.cs
+++ Controllers/PermissionController.cs
@@ -15,15 +15,52 @@ namespace ECommerce1.Controllers
     public class PermissionController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+
         public PermissionController(RoleManager<IdentityRole> roleManager)
         {
             _roleManager = roleManager;
         }
+
+        [HttpGet]
         public async Task<ActionResult> Index(string id)
         {
             var model = new PermissionViewModel();
             var allPermissions = new List<RoleClaimsViewModel>();
-            allPermissions.GetPermissions(typeof(Permissions.Products), id);
+            var listPermissions = new List<RoleClaimsViewModel>();
+
+            listPermissions.GetPermissions(typeof(Permissions.Dashboard), id);
+            allPermissions.AddRange(listPermissions);
+
+            listPermissions.GetPermissions(typeof(Permissions.Customers), id);
+            allPermissions.AddRange(listPermissions);
+
+            listPermissions.GetPermissions(typeof(Permissions.Employees), id);
+            allPermissions.AddRange(listPermissions);
+
+            listPermissions.GetPermissions(typeof(Permissions.Roles), id);
+            allPermissions.AddRange(listPermissions);
+
+            listPermissions.GetPermissions(typeof(Permissions.Orders), id);
+            allPermissions.AddRange(listPermissions);
+
+            listPermissions.GetPermissions(typeof(Permissions.Returns), id);
+            allPermissions.AddRange(listPermissions);
+
+            listPermissions.GetPermissions(typeof(Permissions.Products), id);
+            allPermissions.AddRange(listPermissions);
+
+            listPermissions.GetPermissions(typeof(Permissions.PromosAndDiscounts), id);
+            allPermissions.AddRange(listPermissions);
+
+            listPermissions.GetPermissions(typeof(Permissions.Reports), id);
+            allPermissions.AddRange(listPermissions);
+
+            listPermissions.GetPermissions(typeof(Permissions.AboutUs), id);
+            allPermissions.AddRange(listPermissions);
+
+            listPermissions.GetPermissions(typeof(Permissions.ContactUs), id);
+            allPermissions.AddRange(listPermissions);
+
             var role = await _roleManager.FindByIdAsync(id);
             model.RoleId = id;
             var claims = await _roleManager.GetClaimsAsync(role);
@@ -40,20 +77,22 @@ namespace ECommerce1.Controllers
             model.RoleClaims = allPermissions;
             return View(model);
         }
-        //public async Task<IActionResult> Update(PermissionViewModel model)
-        //{
-        //    var role = await _roleManager.FindByIdAsync(model.id);
-        //    var claims = await _roleManager.GetClaimsAsync(role);
-        //    foreach (var claim in claims)
-        //    {
-        //        await _roleManager.RemoveClaimAsync(role, claim);
-        //    }
-        //    var selectedClaims = model.RoleClaims.Where(a => a.Selected).ToList();
-        //    foreach (var claim in selectedClaims)
-        //    {
-        //        await _roleManager.AddPermissionClaim(role, claim.Value);
-        //    }
-        //    return RedirectToAction("Index", new { id = model.id });
-        //}
+
+        [HttpPost]
+        public async Task<IActionResult> UpdatePermission([Bind] PermissionViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.RoleId);
+            var claims = await _roleManager.GetClaimsAsync(role);
+            foreach (var claim in claims)
+            {
+                await _roleManager.RemoveClaimAsync(role, claim);
+            }
+            var selectedClaims = model.RoleClaims.Where(a => a.Selected).ToList();
+            foreach (var claim in selectedClaims)
+            {
+                await _roleManager.AddPermissionClaim(role, claim.Value);
+            }
+            return RedirectToAction("Index", "Roles");
+        }
     }
 }
--- Data/Permissions.cs
+++ Data/Permissions.cs
@@ -49,9 +49,59 @@ namespace ECommerce1.Data
             return _results;
         }
 
+        public static class AboutUs
+        {
+            public const string Edit = "Permissions.AboutUs.Edit";
+            public const string View = "Permissions.AboutUs.View";
+        }
+
+        public static class ContactUs
+        {
+            public const string Edit = "Permissions.ContactUs.Edit";
+            public const string View = "Permissions.ContactUs.View";
+        }
+
+        public static class Customers
+        {
+            public const string Create = "Permissions.Customers.Create";
+            public const string Delete = "Permissions.Customers.Delete";
+            public const string Edit = "Permissions.Customers.Edit";
+            public const string View = "Permissions.Customers.View";
+        }
+
         public static class Dashboard
         {
             public const string View = "Permissions.Dashboard.View";
+        }
+
+        public static class Employees
+        {
+            public const string Create = "Permissions.Employees.Create";
+            public const string Delete = "Permissions.Employees.Delete";
+            public const string Edit = "Permissions.Employees.Edit";
+            public const string View = "Permissions.Employees.View";
+        }
+
+        public static class Orders
+        {
+            public const string Edit = "Permissions.Orders.Edit";
+            public const string View = "Permissions.Orders.View";
+        }
+
+        public static class Products
+        {
+            public const string Create = "Permissions.Products.Create";
+            public const string Delete = "Permissions.Products.Delete";
+            public const string Edit = "Permissions.Products.Edit";
+            public const string View = "Permissions.Products.View";
+        }
+
+        public static class PromosAndDiscounts
+        {
+            public const string Create = "Permissions.PromosAndDiscounts.Create";
+            public const string Delete = "Permissions.PromosAndDiscounts.Delete";
+            public const string Edit = "Permissions.PromosAndDiscounts.Edit";
+            public const string View = "Permissions.PromosAndDiscounts.View";
         }
 
         public static class Reports
@@ -59,60 +109,18 @@ namespace ECommerce1.Data
             public const string View = "Permissions.Reports.View";
         }
 
-        public static class Orders
-        {
-            public const string View = "Permissions.Orders.View";
-            public const string Edit = "Permissions.Orders.Edit";
-        }
-
         public static class Returns
         {
-            public const string View = "Permissions.Returns.View";
             public const string Edit = "Permissions.Returns.Edit";
-        }
-
-        public static class AboutUs
-        {
-            public const string View = "Permissions.AboutUs.View";
-            public const string Edit = "Permissions.AboutUs.Edit";
-        }
-
-        public static class ContactUs
-        {
-            public const string View = "Permissions.ContactUs.View";
-            public const string Edit = "Permissions.ContactUs.Edit";
-        }
-
-        public static class Customers
-        {
-            public const string View = "Permissions.Customers.View";
-            public const string Create = "Permissions.Customers.Create";
-            public const string Edit = "Permissions.Customers.Edit";
-            public const string Delete = "Permissions.Customers.Delete";
-        }
-
-        public static class Employees
-        {
-            public const string View = "Permissions.Employees.View";
-            public const string Create = "Permissions.Employees.Create";
-            public const string Edit = "Permissions.Employees.Edit";
-            public const string Delete = "Permissions.Employees.Delete";
+            public const string View = "Permissions.Returns.View";
         }
 
         public static class Roles
         {
-            public const string View = "Permissions.Roles.View";
             public const string Create = "Permissions.Roles.Create";
-            public const string Edit = "Permissions.Roles.Edit";
             public const string Delete = "Permissions.Roles.Delete";
-        }
-
-        public static class Products
-        {
-            public const string View = "Permissions.Products.View";
-            public const string Create = "Permissions.Products.Create";
-            public const string Edit = "Permissions.Products.Edit";
-            public const string Delete = "Permissions.Products.Delete";
+            public const string Edit = "Permissions.Roles.Edit";
+            public const string View = "Permissions.Roles.View";
         }
     }
 }
